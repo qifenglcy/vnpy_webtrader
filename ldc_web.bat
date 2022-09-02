d183b1ee6311fe1e747fe2bf4f2c16b03bd2f98a
qifeng <2575562250@qq.com>
2022-09-02 12:32:50 +0800

    lcy2

--- ldc_web.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-start cmd /k "cd /d C:\Users\qifeng&&python run_ui_web_rpc.py"
-timeout /T 10
-start cmd /k "cd /d C:\Users\qifeng\vnpy_web &&uvicorn main:app --host=0.0.0.0 --port=8000 --reload"
-
-
